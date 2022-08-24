26936ffa9d23c129810e8c5c0e1299fe657bc832
homayoun <homayounfalahati@gmail.com>
2022-08-24 20:24:22 +0430

    added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000);
-
